a850823d723a51fb4f109f625b43d125d253be76
Amos  Vryhof <amos@vryhofresearch.com>
2021-07-21 07:34:17 -0400

    Start to flesh out scanner class

--- get_item.py
+++ get_item.py
@@ -6,34 +6,31 @@ After initialization, try waving various 13.56MHz RFID cards over it!
 """
 
 import RPi.GPIO as GPIO
-from pygame import mixer
 
+from beep_class import beep
 from pn532 import *
 
-if __name__ == '__main__':
-    mixer.init()
-    alert = mixer.Sound('beep.wav')
-
+if __name__ == "__main__":
     try:
         pn532 = PN532_SPI(debug=False, reset=20, cs=4)
         # pn532 = PN532_I2C(debug=False, reset=20, req=16)
         # pn532 = PN532_UART(debug=False, reset=20)
 
         ic, ver, rev, support = pn532.get_firmware_version()
-        print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
+        print("Found PN532 with firmware version: {0}.{1}".format(ver, rev))
 
         # Configure PN532 to communicate with MiFare cards
         pn532.SAM_configuration()
 
-        print('Waiting for RFID/NFC card...')
+        print("Waiting for RFID/NFC card...")
         while True:
             # Check if a card is available to read
             uid = pn532.read_passive_target(timeout=0.5)
-            print('.', end="")
+            print(".", end="")
             # Try again if no card is available.
             if uid is not None:
-                alert.play()
-                print('Found card with UID:', "".join([hex(i) for i in uid]))
+                beep()
+                print("Found card with UID:", "".join([hex(i) for i in uid]))
 
             else:
                 continue
---
+++ scanner.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+
+from beep_class import beep
+from pn532 import PN532_SPI
+
+DEBUG = False
+
+
+class RFIDScanner(object):
+    scanner = None
+
+    def __init__(self):
+        self.scanner = PN532_SPI(debug=False, reset=20, cs=4)
+
+        if DEBUG:
+            ic, ver, rev, support = self.scanner.get_firmware_version()
+            print("Found PN532 with firmware version: {0}.{1}".format(ver, rev))
+
+        self.scanner.SAM_configuration()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        GPIO.cleanup()
+
+    def wait_for_scan(self):
+        if DEBUG:
+            print("Waiting for RFID/NFC card...")
+
+        uid = None
+        while uid is None:
+            uid = self.scanner.read_passive_target(timeout=0.5)
+
+        uid_string = "".join([hex(i) for i in uid])
+        beep()
+        if DEBUG:
+            print("Found card with UID: {}".format(uid_string))
